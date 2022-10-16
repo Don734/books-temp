993ec0c4b059d7d6a34f9caf431fbd97c29da28d
don734 <vip.don4ik@bk.ru>
2022-10-16 20:32:58 +0500

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     swiperSlider();
+    productGallery();
 });
 
 
@@ -11,4 +12,26 @@ function swiperSlider() {
         spaceBetween: 30,
     };
     const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function productGallery() {
+    let swiperImgContainer = '.swiper-product-img';
+    let swiperThumbContainer = '.swiper-product-thumbs';
+    
+    let swiperThumbOptions = {
+        spaceBetween: 5,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    };
+    const swiperProductThumb = new Swiper(swiperThumbContainer, swiperThumbOptions);
+
+    let swiperImgOptions = {
+        spaceBetween: 5,
+        thumbs: {
+            swiper: swiperProductThumb,
+        }
+    };
+
+    const swiperProductImg = new Swiper(swiperImgContainer, swiperImgOptions);
 }
